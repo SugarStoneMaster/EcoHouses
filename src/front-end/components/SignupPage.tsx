408977b6365e78343cc7e5bc241b4b8f75043d6f
antoniopicarella <antonio.picarella.101997@gmail.com>
2024-12-01 14:22:06 +0100

    FIXING

--- src/front-end/components/SignupPage.tsx
+++ src/front-end/components/SignupPage.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
-import CheckBox from '@react-native-community/checkbox';
+import CheckBox from "@react-native-community/checkbox";
 import Icon from 'react-native-vector-icons/FontAwesome';
-import styles from './SignupPage';
-import SignupPageStyles from "../styling/SignupPageStyles"; // Import degli stili separati
+import SignupPageStyles from '../styling/SignupPageStyles'; // Import degli stili corretti
 
 const SignupPage = () => {
     const [username, setUsername] = useState('');
@@ -11,6 +10,7 @@ const SignupPage = () => {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [isManager, setIsManager] = useState(false);
+
 
     const handleSignup = () => {
         // Logica di registrazione
@@ -25,54 +25,74 @@ const SignupPage = () => {
 
     return (
         <View style={SignupPageStyles.container}>
-            <Text style={SignupPageStyles.title}>Benvenuto su EcoHouses!</Text>
+            {/* Elementi decorativi */}
+            <View style={{ position: 'absolute', top: -50, left: -50, width: 200, height: 200, backgroundColor: '#CDE8D9', borderRadius: 100, zIndex: -1 }} />
+            <View style={{ position: 'absolute', top: 100, right: -70, width: 150, height: 150, backgroundColor: '#A8D5BA', borderRadius: 75, zIndex: -1 }} />
+
+            <Text style={SignupPageStyles.title}>Benvenuto in EcoHouses!</Text>
+
+            {/* Input Nome Utente */}
             <View style={SignupPageStyles.inputContainer}>
-                <Icon name="user" size={20} color="black" style={SignupPageStyles.icon} />
+                <Icon name="user" size={20} style={SignupPageStyles.icon} />
                 <TextInput
                     style={SignupPageStyles.input}
                     placeholder="Nome utente"
-                    placeholderTextColor="black"
+                    placeholderTextColor="grey"
                     value={username}
                     onChangeText={setUsername}
                 />
             </View>
+
+            {/* Input Email */}
             <View style={SignupPageStyles.inputContainer}>
-                <Icon name="envelope" size={20} color="black" style={SignupPageStyles.icon} />
+                <Icon name="envelope" size={20} style={SignupPageStyles.icon} />
                 <TextInput
                     style={SignupPageStyles.input}
                     placeholder="Email"
-                    placeholderTextColor="black"
+                    placeholderTextColor="#888"
                     keyboardType="email-address"
                     value={email}
                     onChangeText={setEmail}
                 />
             </View>
+
+            {/* Input Password */}
             <View style={SignupPageStyles.inputContainer}>
-                <Icon name="lock" size={20} color="black" style={SignupPageStyles.icon} />
+                <Icon name="lock" size={20} style={SignupPageStyles.icon} />
                 <TextInput
                     style={SignupPageStyles.input}
                     placeholder="Password"
-                    placeholderTextColor="black"
+                    placeholderTextColor="grey"
                     secureTextEntry
                     value={password}
                     onChangeText={setPassword}
                 />
             </View>
+
+            {/* Input Conferma Password */}
             <View style={SignupPageStyles.inputContainer}>
-                <Icon name="lock" size={20} color="black" style={SignupPageStyles.icon} />
+                <Icon name="lock" size={20} style={SignupPageStyles.icon} />
                 <TextInput
                     style={SignupPageStyles.input}
                     placeholder="Conferma password"
-                    placeholderTextColor="black"
+                    placeholderTextColor="#888"
                     secureTextEntry
                     value={confirmPassword}
                     onChangeText={setConfirmPassword}
                 />
             </View>
+
+            {/* Checkbox Gestore */}
             <View style={SignupPageStyles.checkboxContainer}>
-                <CheckBox value={isManager} onValueChange={setIsManager} />
+                <CheckBox
+                    value={isManager}
+                    onValueChange={setIsManager}
+                    boxType="circle"
+                />
                 <Text style={SignupPageStyles.checkboxLabel}>Sono il gestore della mia abitazione</Text>
             </View>
+
+            {/* Bottone Registrati */}
             <TouchableOpacity style={SignupPageStyles.button} onPress={handleSignup}>
                 <Text style={SignupPageStyles.buttonText}>Registrati</Text>
             </TouchableOpacity>
--- src/front-end/styling/SignupPageStyles.tsx
+++ src/front-end/styling/SignupPageStyles.tsx
@@ -5,50 +5,61 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     padding: 16,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: '#E9F5F0', // Sfondo verde chiaro
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
-    marginBottom: 16,
+    marginBottom: 20,
+    color: '#000', // Colore del testo nero
   },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 12,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    marginBottom: 16,
+    backgroundColor: '#CDE8D9', // Colore di sfondo degli input
+    borderRadius: 25, // Arrotondamento
+    paddingHorizontal: 10,
+    height: 50,
   },
   icon: {
     marginRight: 8,
+    color: '#000', // Icone nere
   },
   input: {
     flex: 1,
-    height: 40,
     fontSize: 16,
-    color: 'black',
+    color: '#000', // Testo nero
   },
   checkboxContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginVertical: 12,
+    marginVertical: 16,
   },
   checkboxLabel: {
     fontSize: 16,
     marginLeft: 8,
+    color: '#000', // Testo nero
   },
   button: {
-    backgroundColor: '#007BFF',
-    padding: 12,
-    borderRadius: 4,
+    backgroundColor: '#5DADE2', // Celeste
+    padding: 15,
+    borderRadius: 25, // Arrotondamento
     alignItems: 'center',
+    shadowColor: '#000', // Ombra
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5, // Ombra su Android
   },
   buttonText: {
-    color: '#fff',
-    fontSize: 16,
+    color: '#fff', // Testo bianco
+    fontSize: 18,
     fontWeight: 'bold',
   },
 });
 
 export default styles;
+
+
