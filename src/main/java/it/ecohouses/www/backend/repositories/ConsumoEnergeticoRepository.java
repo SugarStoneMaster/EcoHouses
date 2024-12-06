631b76af032f3cfd33d211682fd7bc0a334bddf9
Maria Antonietta Cervo <maryantony.913@gmail.com>
2024-12-06 12:03:40 +0100

    Aggiunta funzionalità back-end visualizzazione media consumi per utenti simili

--- src/main/java/it/ecohouses/www/backend/repositories/ConsumoEnergeticoRepository.java
+++ src/main/java/it/ecohouses/www/backend/repositories/ConsumoEnergeticoRepository.java
@@ -5,9 +5,28 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ConsumoEnergeticoRepository extends JpaRepository<ConsumoEnergetico, Long> {
     @Query("SELECT c FROM ConsumoEnergetico c WHERE c.abitazione.idAbitazione = :idAbitazione")
     List<ConsumoEnergetico> findConsumiByAbitazione(@Param("idAbitazione") Long idAbitazione);
+
+    @Query("SELECT AVG(c.valoreConsumo) FROM ConsumoEnergetico c " +
+            "JOIN c.abitazione a " +
+            "WHERE a.comune = :comune " +
+            "AND a.metratura BETWEEN :superficieMin AND :superficieMax " +
+            "AND a.numeroPersone = :numeroPersone " +
+            "AND a.classeEnergetica = :classeEnergetica " +
+            "AND c.data BETWEEN :dataInizio AND :dataFine")
+    Double findMediaConsumiByComuneCaratteristicheEPeriodo(
+            @Param("comune") String comune,
+            @Param("superficieMin") Double superficieMin,
+            @Param("superficieMax") Double superficieMax,
+            @Param("numeroPersone") Integer numeroPersone,
+            @Param("classeEnergetica") String classeEnergetica,
+            @Param("dataInizio") LocalDateTime dataInizio,
+            @Param("dataFine") LocalDateTime dataFine);
+
+
 }
