c193b95bc937cb6b1c061d12380b1490b5b3fdf6
Roberto Polito <politorobertolavoro@gmail.com>
2024-12-14 20:17:16 +0100

    Testing Iot e Social

--- src/main/java/it/ecohouses/www/backend/model/SmartMeter.java
+++ src/main/java/it/ecohouses/www/backend/model/SmartMeter.java
@@ -2,17 +2,14 @@ package it.ecohouses.www.backend.model;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class SmartMeter extends DispositivoIoT {
 
     private String email;
     private String password;
 
-    public SmartMeter(String email, String password) {
-        super();
-        this.email = email;
-        this.password = password;
-    }
 }
--- src/main/java/it/ecohouses/www/backend/model/Utente.java
+++ src/main/java/it/ecohouses/www/backend/model/Utente.java
@@ -76,4 +76,5 @@ public class Utente {
                 ", gestore=" + gestore +
                 '}';
     }
+
 }
